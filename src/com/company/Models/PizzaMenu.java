bc0c409ab5db74c42de80a5739d7d1b45822cf56
Matvei Belousov <bel.mot50@gmail.com>
2021-09-20 12:40:13 +0200

    nova trida (2)

---
+++ src/com/company/Models/PizzaMenu.java
@@ -0,0 +1,32 @@
+package com.company.Models;
+
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PizzaMenu {
+    private List<PizzaMenuItem> items;
+    private LocalDateTime  lastUpdate;
+
+    public PizzaMenu() {
+        items = new ArrayList<>();
+    }
+
+    public List<PizzaMenuItem> getItems() {
+        return items;
+    }
+
+    public void addItem(PizzaMenuItem item) {
+        items.add(item);
+    }
+
+    public int itemsCount() {
+        return items.size();
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+}
+
