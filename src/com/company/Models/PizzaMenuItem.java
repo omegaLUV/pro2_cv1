b09a3deb3cf8c0db2688803bea46145aa6ec64e8
Matvei Belousov <bel.mot50@gmail.com>
2021-09-20 12:29:53 +0200

    nova trida

---
+++ src/com/company/Models/PizzaMenuItem.java
@@ -0,0 +1,37 @@
+package com.company.Models;
+
+public class PizzaMenuItem {
+    private String name;
+    private String description;
+    private int price;
+
+    public PizzaMenuItem (String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
